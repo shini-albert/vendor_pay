414ad60a18d1731c555ee53d1a8f9c2ed7216c94
shini-albert <shini@cygnetin.com>
2026-09-23 12:38:43 +0530

    initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    //
+    public function test()
+    {
+        return view('test');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\TestpageController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test',[TestpageController::class,'test']
+);
