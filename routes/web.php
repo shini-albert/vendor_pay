1493ce8a2f6c2fd6d18af56c491fa2f81c4bbe57
Seniya Najeem <seniyanajeem1@gmail.com>
2026-09-24 12:55:39 +0530

    Login

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
 
-use App\Http\Controllers\Controller;
-use App\Http\Controllers\TestpageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PaymentController;
 
+// Guest Routes
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+// Authenticated Routes
 Route::middleware('auth')->group(function () {
-    Route::get('/test', [AuthController::class, 'showTestPage'])->name('test');
+    Route::get('/payment', [PaymentController::class, 'create'])->name('payment');
+    Route::post('/payments', [PaymentController::class, 'store'])->name('payment.store');
 });
-Route::get('/test',[TestpageController::class,'test']
-
-);
-Route::get('/payment', [PaymentController::class, 'create'])->name('payment');
-Route::post('/payments', [PaymentController::class, 'store'])->name('payment.store');
