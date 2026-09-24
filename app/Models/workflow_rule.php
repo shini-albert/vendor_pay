e23ae32537949127228f313f6669acefa124ce53
shini-albert <shini@cygnetin.com>
2026-09-24 12:48:05 +0530

    newclass

--- app/Models/workflow_rule.php
+++ app/Models/workflow_rule.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class workflow_rule extends Model
 {
-    //
+    public function workflow()
+    {
+        return $this->belongsTo(workflow::class);
+    }
+     public function workflowsteps()
+    {
+        return $this->hasMany(Workflow_Step::class);
+    }
 }
---
+++ database/seeders/workflow_stepSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class workflow_stepSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
